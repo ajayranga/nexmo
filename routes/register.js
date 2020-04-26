1589e823832ea26319c5bfa6ec1e0a5979d16e88
Mr Unique <ajay.ranga.mr.unique@gmail.com>
2020-04-26 21:37:55 +0530

    Nexmo

---
+++ routes/register.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+const express = require('express');
+const router = express.Router();
+const Nexmo = require('nexmo');
+const nexmo = new Nexmo({
+    apiKey: process.env.API_KEY,
+    apiSecret: process.env.API_SECRET
+});
+router.post('/register', (req, res) => {
+    // A user registers with a mobile phone number
+    let phoneNumber = req.body.number;
+    let message = req.body.message;
+    console.log(phoneNumber);
+    nexmo.verify.request({
+        number: '919992750105',
+        brand: 'Vonage',
+        code_length: '4'
+      }, (err, result) => {
+        console.log(err ? err : result)
+      });
+    // nexmo.verify.request({ number: phoneNumber, brand: message, code_length: '4' },
+    //     (err, result) => {
+    //         if (err) {
+    //             //res.sendStatus(500);
+    //             console.log("ERRor",err)
+    //             res.render('status', { message: 'Server Error' });
+    //         } else {
+    //             console.log(result);
+    //             let requestId = result.request_id;
+    //             if (result.status == '0') {
+    //                 res.render('verify', { requestId: requestId });
+    //             } else {
+    //                 //res.status(401).send(result.error_text);
+    //                 res.render('status', {
+    //                     message: result.error_text,
+    //                     requestId: requestId
+    //                 });
+    //             }
+    //         }
+    //     });
+});
+module.exports = router;
---
+++ routes/status.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+/* GET home page. */
+router.get('/status', function (req, res, next) {
+    res.render('status');
+});
+module.exports = router;
